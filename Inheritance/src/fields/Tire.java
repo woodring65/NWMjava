473b4cb13daa84029ae2fc93c8130841f0ca8492
woodring <nss20970029@gmail.com>
2021-04-27 14:22:11 +0900

    [Inheritance]fileds: 필드의 다형성

---
+++ Inheritance/src/fields/Tire.java
@@ -0,0 +1,30 @@
+package fields;
+
+public class Tire {
+	
+	public int maxRotation;
+	public int accumulatedRotation;
+	public String location;
+	
+	//생성자
+	public Tire(String location, int maxRotation) {
+		this.location = location;
+		this.maxRotation = maxRotation;
+	}
+	
+	
+	//메소드
+	public boolean roll() {
+		++accumulatedRotation;
+		if(accumulatedRotation < maxRotation) {
+			System.out.println(location + " Tire 수명: " +
+		(maxRotation - accumulatedRotation) + "회");
+		return true;	
+		}
+		else {
+			System.out.println("*** " + location + "Tire 펑크 ***");
+			return false;
+		}
+	}
+
+}
